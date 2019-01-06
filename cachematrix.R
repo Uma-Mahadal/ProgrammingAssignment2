c3c7d47c6d268d4b071a788f9943af88486c2115
Uma-Mahadal <37560605+Uma-Mahadal@users.noreply.github.com>
2019-01-06 17:13:10 -0500

    Add files via upload
    Assignment Solution for Peer grading

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,64 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## cachematrix.R
+## source("cachematrix.R") - usage
+## myMatrix <- makeCacheMatrix(matrix(0:4, 4, 4)
+## myMatrix$getMatrix()
+## myMatrix$getCache() - will return NULL for the first time
+## cacheSolve(myMatrix)
+## To view the solution use - myMatrix$getCache() 
 
 makeCacheMatrix <- function(x = matrix()) {
 
+cacheMatrix <- NULL
+
+# set the matrix - setMatrix method
+setMatrix <- function(y)
+{
+	x <<- y
+	cacheMatrix <<- NULL
+}
+
+#define getMatrix now
+
+getMatrix <- function() x
+
+# Next, setCache
+
+setCache <- function(inverse) cacheMatrix <<- inverse
+
+# Now, defining the getCache to return the cached inverse of x
+
+getCache <- function() cacheMatrix
+
+# as per the makevector example, now list the names of all methods
+
+list(setMatrix = setMatrix,
+	getMatrix = getMatrix,
+	setCache = setCache,
+	getCache = getCache)
+
+## end
+
+
 }
 
 
-## Write a short comment describing this function
+## To solve and Return a matrix that is the inverse of 'x'
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        cacheMatrix <- x$getCache()
+
+# handle null values
+	if(!is.null(cacheMatrix)) {
+		message("Processing...")
+		return(cacheMatrix)
+      }
+	else {
+		nMatrix <- x$getMatrix()
+		cacheMatrix <- solve(nMatrix, ...)
+		x$setCache(cacheMatrix)
+		return(cacheMatrix)
+	}
 }
+
+
